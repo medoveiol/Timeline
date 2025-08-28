b5cb6bed0926ddec354be23f39e468c718fcc612
rahma <71207744+medoveiol@users.noreply.github.com>
2025-08-28 21:38:59 +0300

    Update Timeline.js

--- Timeline.js
+++ Timeline.js
@@ -43,7 +43,7 @@ const ALL_EVENTS = [
   { id:"www91", title:"World Wide Web",  year:1991, hint:"The modern web begins",  img:"images/World.png" },
   { id:"google98", title:"Google Founded", year:1998, hint:"Search revolution",     img:"images/Google.svg" },
   { id:"msn99", title:"MSN Messenger", year:1999, hint:"Instant messaging era", img:"images/msn.svg" },
-  { id:"fb04", title:"Facebook Launch",   year:2004, hint:"Mainstream social media",img:"images/facebook.png" },
+  { id:"fb04", title:"Facebook Launch",   year:2004, hint:"social media era",img:"images/facebook.png" },
   { id:"bb05", title:"BlackBerry Smartphone", year:2005, hint:"BBM era", img:"images/blackb.svg" },
   { id:"iphone07", title:"First iPhone",  year:2007, hint:"Smartphone era",         img:"images/frist.png" },
   { id:"chatgpt22", title:"ChatGPT Launch",year:2022, hint:"Generative AI",         img:"images/ChatG.png" }
@@ -291,6 +291,7 @@ resetBtn.addEventListener('click', ()=>{
 
 window.addEventListener('load', adjustTimeline);
 window.addEventListener('resize', adjustTimeline);
+
 
 
 
